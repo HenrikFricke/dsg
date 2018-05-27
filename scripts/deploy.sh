a6c394dc402d5a2e9c330dea42fc17fff9797aee
Henrik Fricke <henrik.fricke@jimdo.com>
2018-05-27 20:39:00 +0200

    Fix deployment script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,7 @@ now -e GITHUB_TOKEN=@github_token \
   -e DSG_BASE_URL=https://dsg.now.sh \
   -e CLOUDFLARE_API_KEY=@cloudflareapikey \
   -e CLOUDFLARE_AUTH_EMAIL=@cloudflareauthemail \
-  -e CLOUDFLARE_PURGE_CACHE_ENDPOINT=@cloudflarepurgecacheendpoint
+  -e CLOUDFLARE_PURGE_CACHE_ENDPOINT=@cloudflarepurgecacheendpoint \
   -e WEBHOOK_PURGE_SECRET=@webhookpurgesecret
 now alias
 now rm dsg -s -y
