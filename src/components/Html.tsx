6cbe6682ba4c12671c9d27b45decbcb0d918d5bf
Henrik Fricke <henrik.fricke@jimdo.com>
2018-05-28 11:26:02 +0200

    Add css file to head

--- src/components/Html.tsx
+++ src/components/Html.tsx
@@ -9,6 +9,7 @@ export const Html: React.StatelessComponent<Props> = props => {
     <html>
       <head>
         <title>Home | Dynamic Site Generator</title>
+        <link rel="stylesheet" href="assets/styling.css" type="text/css" />
       </head>
       <body dangerouslySetInnerHTML={{ __html: props.content }} />
     </html>
